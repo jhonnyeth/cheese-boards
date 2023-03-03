6aae8de98625e2b5c419a2edd32bf638e27d2af6
jhonnyeth <jasvofficial@gmail.com>
2023-03-03 15:59:32 -0500

    adding data

--- codetest.js
+++ codetest.js
@@ -3,3 +3,11 @@ const sequelize = require("./index");
 const User = require("./models/User");
 const Board = require("./models/Board");
 const Cheese = require("./models/Cheese");
+
+User.hasMany(Board);
+Board.belongsTo(User);
+
+Board.belongsToMany(Cheese, { through: "cheese_boards" });
+Cheese.belongsToMany(Board, { through: "cheese_boards" });
+
+//DATA//
